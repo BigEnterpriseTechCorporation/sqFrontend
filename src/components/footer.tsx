d51fc604f7f140e8d8bb897ee8440fb8a5df7d22
KonstantinHerasimenko <konstantin.herasimenko@gmail.com>
2025-04-19 13:17:38 +0300

    feat: add /units and /unit/[id] styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,22 @@
 import Logo from "@/assets/icons/logo.svg";
+import WaveDark from "@/assets/icons/wave-dark.svg";
 import Link from "next/link";
 
 
 export default function Footer(){
     return (
         <footer className="text-xl font-jura font-bold pb-32 pt-10 px-20 relative bg-[#202020] text-white relative">
+            {/* Wave at the top of footer */}
+            <WaveDark className="absolute left-0 top-0 w-full transform -translate-y-full"/>
+            
             {/* Navigation Column */}
             <div>
                 <h2 className="text-4xl font-jura mb-8">Навигация</h2>
-                <nav className="flex flex-col space-y-4 font-raleway hover:text-gray-300 text-xl font-normal">
-                    <Link href="/auth">Регистрация</Link>
-                    <Link href="/login">Log in</Link>
-                    <Link href="/units">Темы</Link>
-                    <Link href="/">Профиль</Link>
+                <nav className="flex flex-col space-y-4 font-raleway text-xl font-normal">
+                    <Link className={"hover:text-gray-300"} href="/auth">Регистрация</Link>
+                    <Link className={"hover:text-gray-300"} href="/login">Log in</Link>
+                    <Link className={"hover:text-gray-300"} href="/units">Темы</Link>
+                    <Link className={"hover:text-gray-300"} href="/">Профиль</Link>
                 </nav>
             </div>
             {/* Logo Column */}
--- src/components/unitTitle.tsx
+++ src/components/unitTitle.tsx
@@ -8,7 +8,7 @@ export default function UnitTitle({title,...props}:UnitTitleProps){
     return(
         <div className="bg-[#fcf7e6] pt-12 pb-8 relative" {...props}>
             <div className="max-w-5xl mx-auto px-4">
-                <h1 className="text-5xl text-center font-bold pb-20 pt-10">{title}</h1>
+                <h1 className=" text-9xl text-center font-bold pb-20 pt-4 font-jura">{title}</h1>
             </div>
             {/* Wavy border */}
             <WavePink className={"absolute bottom-0 w-full"}/>
